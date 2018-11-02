acabd87d86f02f597a7d1933f2dc6fd890925903
AnotherPotato <joansebastian35@gmail.com>
2018-11-02 04:50:02 -0500

    Commit inicial:

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const databaseName = 'ghost-website';
+const client = new MongoClient(url);
+var database = null;
+
+client.connect(function(err){
+    if(err){
+        console.error(err);
+        return;
+    }
+
+    database = client.db(databaseName);
+});
+
+var app = express();
+
+app.use(express.static('public') );
+
+app.engine('handlebars', hbs() );
+app.set('viewengine', 'handlebars');
+
+app.use(express.static('public'));
+app.engine('handlebars', hbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function(request, response){
+    const collection = database.collection('products');
+    collection.find({}).toArray(function(err, docs){
+        if(err) {
+            console.error(err);
+            response.send(err);
+            return;
+        }
+        var context = {
+            products: docs,
+        }
+        response.render('home', context);
+    });
+});
+
+app.get('/addItemToCart', function(request, response){
+    const collection = database.collection('products');
+    collection.insert({
+        name: 'Prequelle CD',
+        price: 15,
+    }, function(err, result){
+        if(err) {
+            console.error(err);
+            response.send(err);
+            return;
+        }
+        response.send('Item Agregado');
+    });
+});
+
+app.listen(5500);
